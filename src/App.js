f03c72b7eaf691b15a3b6665b4a1697271447edb
SadiaSanam <72416961+SadiaSanam@users.noreply.github.com>
2021-04-05 14:52:54 -0400

    Added Axios and API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Sadia Sanam Sarwar on <a href="https://github.com/SadiaSanam/mydictionary">GitHub</a></small>
+        </footer>
+      </div>
     </div>
   );
 }
