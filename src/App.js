f4b536adf51080dc70b26c26ca75a2e1af71bb2c
SadiaSanam <72416961+SadiaSanam@users.noreply.github.com>
2021-04-05 17:45:10 -0400

    Final touches and updates

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default function App() {
           <img src={logo} className="App-logo img-fluid" alt="logo" />
         </header>
         <main>
-          <Dictionary />
+          <Dictionary defaultKeyword="Aurora" />
         </main>
         <footer className="App-footer">
           <small>Coded by Sadia Sanam Sarwar on <a href="https://github.com/SadiaSanam/mydictionary">GitHub</a></small>
